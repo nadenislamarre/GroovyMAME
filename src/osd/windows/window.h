6b8b7f71e29f74ec77970312051f9630bd25c161
Antonio Giner <calamity15khz@gmail.com>
2020-03-09 20:49:39 +0100

    Preliminar switchres hook (not working yet)

--- src/osd/windows/window.h
+++ src/osd/windows/window.h
@@ -50,6 +50,8 @@ enum class win_window_focus
 };
 
 
+class display_manager;
+
 class win_window_info  : public osd_window_t<HWND>
 {
 public:
@@ -142,6 +144,8 @@ private:
 #endif
 
 	bool                m_attached_mode;
+
+	display_manager *   m_display_manager = 0;
 };
 
 struct osd_draw_callbacks
