2a3c0b8271d1cc8ed828cfc7afc8b33671147b63
Antonio Giner <calamity15khz@gmail.com>
2020-03-26 17:04:31 +0000

    Add preliminar drmkms implementation

--- scripts/src/3rdparty.lua
+++ scripts/src/3rdparty.lua
@@ -2441,6 +2441,12 @@ if _OPTIONS["targetos"]=="linux" then
 		MAME_DIR .. "3rdparty/switchres/display_linux.h",
 		MAME_DIR .. "3rdparty/switchres/custom_video_xrandr.cpp",
 		MAME_DIR .. "3rdparty/switchres/custom_video_xrandr.h",
+		MAME_DIR .. "3rdparty/switchres/custom_video_drmkms.cpp",
+		MAME_DIR .. "3rdparty/switchres/custom_video_drmkms.h",
+	}
+
+	buildoptions {
+		backtick("pkg-config --cflags libdrm"),
 	}
 
 	links {
@@ -2449,5 +2455,5 @@ if _OPTIONS["targetos"]=="linux" then
 
 	local str = backtick("pkg-config --libs libdrm")
 	addlibfromstring(str)
-	addoptionsfromstring(str)	
+	addoptionsfromstring(str)
 end
