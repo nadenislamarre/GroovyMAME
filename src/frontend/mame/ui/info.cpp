7ffbc65876fb015658f4064f1c4a1bbfbafe3d32
Antonio Giner <calamity15khz@gmail.com>
2021-01-03 17:38:41 +0100

    Show Switchres mode in machine's information box

--- src/frontend/mame/ui/info.cpp
+++ src/frontend/mame/ui/info.cpp
@@ -17,6 +17,7 @@
 #include "romload.h"
 #include "softlist.h"
 #include "emuopts.h"
+#include "osdepend.h"
 
 
 namespace ui {
@@ -432,6 +433,7 @@ std::string machine_info::game_info_string() const
 		buf << _("None\n");
 	else
 	{
+		int i = 0;
 		for (screen_device &screen : scriter)
 		{
 			std::string detail;
@@ -454,18 +456,12 @@ std::string machine_info::game_info_string() const
 			util::stream_format(buf,
 					(scrcount > 1) ? _("%1$s: %2$s\n") : _("%2$s\n"),
 					get_screen_desc(screen), detail);
+
+			// display Switchres information
+			buf << _(m_machine.osd().switchres_mode(i++));
 		}
 	}
-/*
-	// display Switchres information
-	modeline *mode = &m_machine.switchres.best_mode;
-	if (mode->hactive)
-	{
-		buf << _("\nSwitchres:\n");
-		util::stream_format(buf, "%d " UTF8_MULTIPLY " %d%s%s %2.3f Hz %2.3f kHz\n",
-			mode->hactive, mode->vactive, mode->interlace?"i":"p", mode->doublescan?"d":"", mode->vfreq, mode->hfreq/1000);
-	}
-*/
+
 	return buf.str();
 }
 
